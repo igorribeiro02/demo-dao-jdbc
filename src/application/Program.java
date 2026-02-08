a65e9437913a71975425b0534674777ac0595ceb
igor ribeiro <igorlopes.ilr@gmail.com>
2026-02-08 16:17:53 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+import java.util.Date;
 
 public class Program {
 
@@ -9,6 +11,8 @@ public class Program {
 		Department obj = new Department(1,"books");
 		System.out.println(obj);
 		
-		
+		Seller seller = new Seller(21, "Bob Brown", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
 	}
 }
